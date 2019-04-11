31fb7ce0ca12b93011002a9f62d9cebef7cfe941
97612336 <xkjskk@163.com>
2019-04-11 17:19:19 +0800

    修改bug

--- apiservers/main/main_api.go
+++ apiservers/main/main_api.go
@@ -12,6 +12,6 @@ import (
 func main(){
 	go heartbeat.ListenHeartbeat()
 	http.HandleFunc("/objects/",objects.Handler)
-	http.HandleFunc("/objects/",locate.Handler)
+	http.HandleFunc("/locate/",locate.Handler)
 	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
 }
